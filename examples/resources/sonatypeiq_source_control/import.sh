0a141f1d0bae58c3332d4eb85cae6b6ab1f647fb
Graham Hargreaves <grahamhar@users.noreply.github.com>
2024-06-21 10:25:30 +0100

    docs: Correct the import documentation (#34)

--- examples/resources/sonatypeiq_source_control/import.sh
+++ examples/resources/sonatypeiq_source_control/import.sh
@@ -2,7 +2,7 @@
 # This can be obtained by searching using the PublicID in the WebUI or by calling the rest API
 
 # Example for an application
-terraform import application/4bb67dcfc86344e3a483832f8c496419
+terraform import sonatypeiq_source_control.application application:4bb67dcfc86344e3a483832f8c496419
 
 # Example for an organization
-terraform import organization/4bb67dcfc86344e3a483832f8c496419
+terraform import sonatypeiq_source_control.organization organization:4bb67dcfc86344e3a483832f8c496419
